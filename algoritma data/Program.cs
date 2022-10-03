5874197d8b1163193945b8a3c873d6dd50d99855
mhmdazwarj <mhmdazwarj@LAPTOP-DQ3Q8RUE>
2022-10-03 17:07:53 +0700

    Add project files.

---
+++ algoritma data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algoritma_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
